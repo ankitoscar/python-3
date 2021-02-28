f93a363d505c363967861e0005dbc9d7c738d8de
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 11:48:04 +0530

    added ex5.py

---
+++ ex5.py
@@ -0,0 +1,24 @@
+# Initializing personal details
+my_name = "Ankit Oscar"
+my_age = 20
+my_height = 173 # in cm
+my_weight = 70 # in kgs
+my_eyes = "Hazel"
+my_hair = "Black"
+
+# Printing  personal details, using f strings
+# Printing name
+print(f"Let's talk about {my_name}")
+
+# Printing height
+print(f"He's {my_height} cm tall")
+
+# Printing weight
+print(f"He's {my_weight} kgs heavy")
+
+# Printing eyes and hair color
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+# Tricky line
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, the total is {total}")
