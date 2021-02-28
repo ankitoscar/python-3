4661a8fdb4bbfb9c952b1df816903db53b546040
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 11:50:48 +0530

    removed my from all variables in ex5.py

--- ex5.py
+++ ex5.py
@@ -1,24 +1,24 @@
 # Initializing personal details
-my_name = "Ankit Oscar"
-my_age = 20
-my_height = 173 # in cm
-my_weight = 70 # in kgs
-my_eyes = "Hazel"
-my_hair = "Black"
+name = "John Smith"
+age = 46
+height = 184 # in cm
+weight = 84 # in kgs
+eyes = "Blue"
+hair = "Blonde"
 
 # Printing  personal details, using f strings
 # Printing name
-print(f"Let's talk about {my_name}")
+print(f"Let's talk about {name}")
 
 # Printing height
-print(f"He's {my_height} cm tall")
+print(f"He's {height} cm tall")
 
 # Printing weight
-print(f"He's {my_weight} kgs heavy")
+print(f"He's {weight} kgs heavy")
 
 # Printing eyes and hair color
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"He's got {eyes} eyes and {hair} hair.")
 
 # Tricky line
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height} and {my_weight}, the total is {total}")
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight}, the total is {total}")
