1fc633510a1c9c3d079b8c38c1405f1761f33da4
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 20:40:26 +0530

    added ex8.py

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# More format string printing
+formatter = "{} {} {} {}"
+
+# printing using this formatter
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "I study",
+    "Electrical Engineering",
+    "at National Institute",
+    "of Technology"
+))
