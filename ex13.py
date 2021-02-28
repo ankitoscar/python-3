193a1f0d239c74ed2188c136d04c4527b7a794a9
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 22:20:54 +0530

    added ex13.py

---
+++ ex13.py
@@ -0,0 +1,12 @@
+# Using argv for using variables as arguments
+# Importing argv from sys
+from sys import argv
+
+# Initialising arguments
+script, first, second, third = argv
+
+# Printing the arguments
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
