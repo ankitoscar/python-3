912ef61fc136b0ba32064a2a8e188a9be4917a8d
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 11:18:56 +0530

    added ex3.py

---
+++ ex3.py
@@ -0,0 +1,32 @@
+# Counting hens and chickens
+print("I will now count my chickens:")
+
+# Number of hens
+print("Hens", 25 + 30 /6)
+
+# Number of roosters
+print("Rooster", 100 - 25 * 3 % 4)
+
+# Counting eggs
+print("Now I will count the eggs:")
+
+# Number of eggs
+print(3 + 2 + 1 - 5 +4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7")
+
+# Printing aforementioned statement
+print(3 + 2 < 5 - 7)
+
+# Silly math statements
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False")
+
+# More of silly statements
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
