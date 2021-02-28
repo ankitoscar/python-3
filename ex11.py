4ae73c4a66fd7f41b313e53dc94b0afbf0ca94a5
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 22:01:21 +0530

    added ex11.py

---
+++ ex11.py
@@ -0,0 +1,15 @@
+# Taking input from the user
+# Asking country
+print("Which country are you from?", end = ' ')
+country = input()
+
+# Asking state
+print("Which state are you from?", end = ' ')
+state = input()
+
+# Asking city
+print("Which city are you from?", end = ' ')
+city = input()
+
+# Printing all variables
+print(f"So, you are from {city}, {state}, {country}.")
