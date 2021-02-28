82c7d00c79401963536788389b267c8a2923dc96
ankitoscar <ankitoscar911@gmail.com>
2021-02-28 22:14:57 +0530

    added ex12.py

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# Declaring, prompting and initializing variables at the same time
+# Taking age
+age = input("How old are you? ")
+
+# Taking height
+height = input("How tall are you? ")
+
+# Taking weight
+weight = input("How much do you weigh? ")
+
+# Printing all input variables
+print(f"Age: {age} years\nHeight: {height} cm\nWeight: {weight} kg")
