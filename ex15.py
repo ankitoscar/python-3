67638d12e1c9dec2447064f9c444fe46961e5b3d
ankitoscar <ankitoscar911@gmail.com>
2021-03-01 20:42:04 +0530

    added ex15.py and its sample txt file

---
+++ ex15.py
@@ -0,0 +1,22 @@
+# Import argv
+from sys import argv
+
+# Passing arguments
+script, filename = argv
+
+# Opening the txt file
+txt = open(filename)
+
+# Printing post import message
+print(f"Here's your file {filename}.")
+print(txt.read())
+
+# Opening the same file again, for no reason
+# Prompt
+print("Type the filename again:")
+file_again = input(">")
+
+# Opening the file again
+txt_again = open(file_again) # Maybe used for opening file without passing as argument
+
+print(txt_again.read())
---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Some strange printing here
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print days and months
+print("Here are days: ", days)
+print("Here are the months: ",months)
+
+# Printing a paragraph using """ """
+print("""
+This is how you
+print a paragraph
+in Python
+""")
