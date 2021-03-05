4c39d64dd923fd9a9a630376a7be6d65ede5c859
ankitoscar <ankitoscar911@gmail.com>
2021-03-05 23:00:08 +0530

    added ex29.py

---
+++ ex29.py
@@ -0,0 +1,32 @@
+# Program with if and else
+# Initialising
+people = 20
+cats = 30
+dogs = 15
+
+# If statements involving cats
+if people < cats :
+    print("Too many cats! The world in doomed")
+
+if people > cats :
+    print("Not many cats! The world is saved!")
+
+# If statements involving dogs
+if people < dogs :
+    print("The world is drooled on!")
+
+if people > dogs :
+    print("The world is dry!")
+
+# Increment dogs by 5
+dogs += 5
+
+# If statements involving dogs
+if people >= dogs :
+    print("People are greater than or equal.")
+
+if people <= dogs :
+    print("People are less than or equal to dogs.")
+
+if people == dogs :
+    print("People are dogs.")
