7500841626218824b23bc006de865510b272481e
ankitoscar <ankitoscar911@gmail.com>
2021-03-13 15:36:05 +0530

    added ex44e.py

---
+++ ex44/ex44e.py
@@ -0,0 +1,33 @@
+# Using composition instead of inheritance
+class Other(object):
+
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other() # composition, passing other class object as a variable
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
