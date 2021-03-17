0e8fc8065b4c9009a36d29a3c0dfe041a74c588e
ankitoscar <ankitoscar911@gmail.com>
2021-03-17 23:34:28 +0530

    modified app_test.py

--- ex50/gothonweb/tests/app_test.py
+++ ex50/gothonweb/tests/app_test.py
@@ -8,12 +8,6 @@ def test_index():
     rv = web.get('/', follow_redirects = True)
     assert_equal(rv.status_code, 200)
 
-def test_index_hello():
-    rv = web.get('/hello', follow_redirects = True)
+def test_game():
+    rv = web.get("/game", follow_redirects = True)
     assert_equal(rv.status_code, 200)
-    assert_in(b"Fill Out this Form", rv.data)
-
-    data = {'name': 'Ankit', 'greet': 'Jai Yeshu'}
-    rv = web.post('/hello', follow_redirects = True, data = data)
-    assert_in(b"Ankit", rv.data)
-    assert_in(b"Jai Yeshu", rv.data)
