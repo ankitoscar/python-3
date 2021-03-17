60c7ed6ca37831fce03650363497b286e72852d0
ankitoscar <ankitoscar911@gmail.com>
2021-03-17 20:59:28 +0530

    modified app.py

--- ex50/gothonweb/app.py
+++ ex50/gothonweb/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     greeting = "Hello! World."
-    return greeting
+    return render_template("index.html", greeting= greeting)
 
 if __name__ == '__main__':
     app.run(debug = True)
